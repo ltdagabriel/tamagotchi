8c0bca78871a79afa4eecbcde8a2d480d8047075
caionakai <kaioesperto@hotmail.com>
2018-05-16 16:17:12 -0300

    banco tamagotchi

--- tabledef.py
+++ tabledef.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
+from datetime import datetime
  
 engine = create_engine('sqlite:///tutorial.db', echo=True)
 Base = declarative_base()
@@ -22,5 +23,19 @@ class User(Base):
         self.username = username
         self.password = password
  
+ class Tamagotchi(Base):
+    __tablename__ = "tamagotchis"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False, ForeignKey('user.id'))
+    name = Column(String)
+    birthday = Column(DateTime, default=datetime.utcnow)
+    last_update = Column(DateTime, default=datetime.utcnow)
+    state = Column(String, default='Saudavel')
+    hunger = Column(Float, default=100.0) 
+    happy = Column(Float, default=100.0) 
+    health = Column(Float, default=100.0)
+
+
 # create tables
 Base.metadata.create_all(engine)
